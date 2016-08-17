662aef1228a9d7e903e27a04d73a25759ff54fad
DrinkaM <rk2211@gmail.com>
2016-08-16 21:15:35 -0400

    Madlibs initial
    Initial short setup for Madlibs

---
+++ Madlibs.rb
@@ -0,0 +1,20 @@
+#User input requests
+
+puts "Gimme a noun!"
+
+a = gets.chomp
+
+puts "Gimme a verb!"
+
+b = gets.chomp
+
+puts "Gimme an adjective!"
+
+c = gets.chomp
+
+puts "Gimme a proper name!"
+
+d = gets.chomp
+
+#Story
+puts "My name is " + d + " and I like to " + b + " on the " + c + " " + a + "!"
