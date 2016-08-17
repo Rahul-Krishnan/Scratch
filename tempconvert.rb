991af4e93413259c4ac61e4190b48c881d0a5f5c
Rahul Krishnan <rk2211@gmail.com>
2016-08-17 01:07:14 -0400

    Conditional Statement added to pick temp convert direction

--- tempconvert.rb
+++ tempconvert.rb
@@ -6,14 +6,22 @@ def f_to_c faren
 	return (faren - 32) * 5/9
 end
 
-# Celsius section
+#Ask for conversion direction
+puts "Convert from C or from F?"
+selection = gets.chomp.downcase
 
-puts "Please enter temp in C"
-c = gets.chomp.to_f
-puts c.to_s + " in Farenheit is: " + c_to_f(c).to_s
+# Celsius section
+if selection.include?('c')
+	puts "Please enter temp in C"
+	c = gets.chomp.to_f
+	puts c.to_s + " in Farenheit is: " + c_to_f(c).to_s
 
 # Farenheit section
+elsif selection.include?('f')
+	puts "Please enter temp in F"
+	f = gets.chomp.to_f
+	puts f.to_s + " in Celsius is: " + f_to_c(f).to_s
 
-puts "Please enter temp in F"
-f = gets.chomp.to_f
-puts f.to_s + " in Celsius is: " + f_to_c(f).to_s
+# Error message
+else puts "Please make sense"
+end
