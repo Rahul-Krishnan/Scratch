fdbb8b7abc39cc0317413b2c4d810cb03879f973
Rahul Krishnan <rk2211@gmail.com>
2016-08-17 01:06:11 -0400

    Extended Story
    Made this more interesting

--- Madlibs.rb
+++ Madlibs.rb
@@ -1,20 +1,32 @@
 #User input requests
 
-puts "Gimme a noun!"
+puts "Gimme the name of a weapon!"
 
-a = gets.chomp
+weapon = gets.chomp.downcase
 
 puts "Gimme a verb!"
 
-b = gets.chomp
+verb = gets.chomp.downcase
 
 puts "Gimme an adjective!"
 
-c = gets.chomp
+adj = gets.chomp.downcase
 
-puts "Gimme a proper name!"
+puts "Gimme a female name!"
 
-d = gets.chomp
+subject_name = gets.chomp.capitalize
+
+puts "Gimme a noun!"
+
+noun = gets.chomp.capitalize
+
+puts "I need a last name!"
+
+firm_name = gets.chomp.capitalize
+
+puts "A final adjective!"
+
+adj2 = gets.chomp.downcase
 
 #Story
-puts "My name is " + d + " and I like to " + b + " on the " + c + " " + a + "!"
+puts "\n" + subject_name + " " + firm_name + "'s Revenge: a Noir Tale. Chapter 1... \n\nI always suspected the mole at " + firm_name + ", " + noun + " and Weinstein was someone related to one of the partners... but the " + adj + " truth now seemed far worse than any of us had feared. \n\nAs she turned to face me, revealing the partially shredded documents on the table, a smirk crossed " + subject_name + "'s face. 'It's a shame you'll never be able to tell my brother about this', she said with unusual confidence as she prepared to make use of the " + adj2 + " " + weapon + " in her hands. In those last frantic moments, all I could do was " + verb + " in desperation.\n\nAnd then, only blackness. " + adj.capitalize + ", " + adj + " blackness."
