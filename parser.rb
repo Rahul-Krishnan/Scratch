5e201cfce153d1a7e2ffa1a3828d61028c4171b7
Rahul Krishnan <rk2211@gmail.com>
2016-09-21 17:51:51 -0400

    Count  links on a webpage

--- parser.rb
+++ parser.rb
@@ -1,2 +1,25 @@
 require 'pry'
+require 'rubygems'
 require 'nokogiri'
+require 'open-uri'
+
+quit = 0
+while quit != "q"
+  puts "Gimme a website:"
+  page_url = gets.chomp
+  if page_url.include?("www.")
+  else
+    page_url = "www.#{page_url}"
+  end
+
+  if page_url.include?("http://")
+  else
+    page_url = "http://#{page_url}"
+  end
+
+  page = Nokogiri::HTML(open(page_url))
+  links = page.css("a")
+  puts "There are #{links.length} links on that page."
+  puts "\nPress Q to Quit, any other key to continue..."
+  quit = gets.chomp
+end
