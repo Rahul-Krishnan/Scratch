c8abd3c0f364547b93d99c47c9cf41eec52c51a4
Rahul Krishnan <rk2211@gmail.com>
2016-09-28 15:41:20 -0400

    Election update overview

---
+++ Election_Update.rb
@@ -0,0 +1,5 @@
+#Take in latest Pollster polling averages
+#Ask for max spread for a swing state
+#Output all swing states with current average spread
+#Display any must-win states for each candidate
+#Display national spread
