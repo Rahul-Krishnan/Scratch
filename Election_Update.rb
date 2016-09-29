37ccbd5075fcdc28036ed239e4064ddcee448219
Rahul Krishnan <rk2211@gmail.com>
2016-09-28 21:38:00 -0400

    wrote out Polls class overview

--- Election_Update.rb
+++ Election_Update.rb
@@ -1,5 +1,19 @@
+class Polls
+#a sorted hash for all the polls
+#a sorted hash for state electoral college votes
+#a swing state hash
+#a national polls hash
+#a display method
+#a method to count paths to 270
+#a method to find must-win states
+#a method to pull in polling data
+end
+
+
 #Take in latest Pollster polling averages
 #Ask for max spread for a swing state
 #Output all swing states with current average spread
 #Display any must-win states for each candidate
 #Display national spread
+
+#show date of last poll for each entry
