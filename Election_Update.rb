4e05be89ce89c8f1125e26d84920db42ee5fd8c6
Rahul Krishnan <rk2211@gmail.com>
2016-10-01 06:17:12 -0400

    Added Princeton as a polls source

--- Election_Update.rb
+++ Election_Update.rb
@@ -4,7 +4,8 @@ require 'nokogiri'
 require 'open-uri'
 require 'httparty'
 require 'date'
-require './poll_eater.rb'
+require './pollster_eater.rb'
+require './princeton_eater.rb'
 
 
 class Poop
@@ -19,34 +20,54 @@ class Poop
 end
 
 puts "Enter State:"
-state = gets.chomp
+state = gets.chomp.upcase
 
 puts "Enter max poll age (days)"
 days = gets.chomp.to_i
 date = (Date.today-days).to_s
 
 
-polls = Polls.new(state,date)
-polls.eat_polls
-polls.calc_averages
+pollster_polls = Pollster::Polls.new(state, date)
+pollster_polls.eat_polls
+
+princeton_polls = Princeton::Polls.new(state, date)
+princeton_polls.eat_polls
 #binding.pry
 
+
+puts "*****Pollster.com results*****"
 puts "Clinton average in polls ending after #{date}:"
-puts polls.averages["clinton"]
+puts pollster_polls.averages["clinton"].round(1)
 puts
 puts "Trump average in polls ending after #{date}:"
-puts polls.averages["trump"]
+puts pollster_polls.averages["trump"].round(1)
 puts
 puts "Johnson average in polls ending after #{date}:"
-puts polls.averages["johnson"]
+puts pollster_polls.averages["johnson"].round(1)
 puts
 puts "Stein average in polls ending after #{date}:"
-puts polls.averages["stein"]
+puts pollster_polls.averages["stein"].round(1)
 puts
 puts "Undecided average in polls ending after #{date}:"
-puts polls.averages["undecided"]
+puts pollster_polls.averages["undecided"].round(1)
+puts
+puts "*****Princeton results*****"
+puts "Clinton average in polls ending after #{date}:"
+puts princeton_polls.averages["clinton"].round(1)
+puts
+puts "Trump average in polls ending after #{date}:"
+puts princeton_polls.averages["trump"].round(1)
+puts
+puts "Other average in polls ending after #{date}:"
+puts princeton_polls.averages["other"].round(1)
+puts
+puts "Undecided average in polls ending after #{date}:"
+puts princeton_polls.averages["undecided"].round(1)
+puts
 
-#Take in latest Pollster polling averages
+#binding.pry
+
+#Take in latest Pollster/Princeton/RCP polling averages
 #Ask for max spread for a swing state
 #Output all swing states with current average spread
 #Display any must-win states for each candidate
---
+++ princeton_eater.rb
@@ -0,0 +1,61 @@
+require 'pry'
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'httparty'
+require 'csv'
+require 'date'
+
+module Princeton
+  class Polls
+
+    attr_accessor :state, :after_date, :scores, :averages, :poll_list
+
+    def initialize state, after_date
+      @state = state
+      @after_date = Date.strptime(after_date, "%Y-%m-%d")
+      @scores = {
+        "clinton" => [],
+        "trump" => [],
+        "other" => [],
+        "undecided" => []
+      }
+      @averages = {
+        "clinton" => 0,
+        "trump" => 0,
+        "other" => 0,
+        "undecided" => 0
+      }
+      @poll_list = []
+    end
+
+
+    def eat_polls
+      data = open("http://election.princeton.edu/code/data/2016_StatePolls.csv")
+      @poll_list = CSV.parse(data.read)
+
+      @poll_list.shift
+      @poll_list.each do |row|
+        if ((row[0] == @state) && (Date.strptime(row[16], "%m/%d/%Y") > @after_date))
+          @scores["trump"] << row[11].to_f
+          @scores["clinton"] << row[12].to_f
+          unless row[13].nil?
+            @scores["other"] << row[13].to_f
+          end
+          unless row[14].nil?
+          @scores["undecided"] << row[14].to_f
+          end
+        else
+        end
+      end
+      calc_averages
+    end
+
+    def calc_averages
+      @averages["clinton"] = @scores["clinton"].reduce(:+)/@scores["clinton"].count unless @scores["clinton"].count == 0
+      @averages["trump"] = @scores["trump"].reduce(:+)/@scores["trump"].count unless @scores["trump"].count == 0
+      @averages["other"] = @scores["other"].reduce(:+)/@scores["other"].count unless @scores["other"].count == 0
+      @averages["undecided"] = @scores["undecided"].reduce(:+)/@scores["undecided"].count unless @scores["undecided"].count == 0
+    end
+  end
+end
